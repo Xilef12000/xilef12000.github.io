6c97bd2218cef74d373b6700083c51195c554266
Xilef12000 <manuel.koenig@online.de>
2021-12-04 12:18:49 +0100

    moved more js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function toggledarklight() {
 	var element = document.body;
 	element.classList.toggle("toggle-darklight");
 }
+function overlay_off() {
+	document.getElementById("overlay").style.display = "none";
+}
