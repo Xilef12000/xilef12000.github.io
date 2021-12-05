9ea5db1491753b729e963100612096a9388e0091
Xilef12000 <manuel.koenig@online.de>
2021-12-05 17:09:02 +0100

    dark favicon when dark mode on

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function onload() {
 	if(!(window.matchMedia('(prefers-color-scheme: dark)').matches)){
-		var element = document.body;
-		element.classList.toggle("toggle-darklight");
+		document.body.classList.toggle("toggle-darklight");
+		document.getElementById("icon").href = "../assets/favicon.svg"
 	}
 }
 function toggledarklight() {
-	var element = document.body;
-	element.classList.toggle("toggle-darklight");
+	document.body.classList.toggle("toggle-darklight");
 }
 function overlay_off() {
 	document.getElementById("overlay").style.display = "none";
--- scripts/404.js
+++ scripts/404.js
@@ -1,5 +1,5 @@
 function onload404() {
+	onload();
 	document.getElementById("Url").innerHTML = "Requested Url: " + window.location.href;
 	document.getElementById("Time").innerHTML = "Time of Request: " + Date();
-	onload();
 }
