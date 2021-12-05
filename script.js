571fb956aac33fb5b272a2ebcc58da64eb1007f3
Xilef12000 <manuel.koenig@online.de>
2021-12-05 17:19:11 +0100

    icon toggles when dark mode gets toggled

--- script.js
+++ script.js
@@ -3,9 +3,18 @@ function onload() {
 		document.body.classList.toggle("toggle-darklight");
 		document.getElementById("icon").href = "../assets/favicon.svg"
 	}
+	else{
+		document.getElementById("icon").href = "../assets/favicon_dark.svg"
+	}
 }
 function toggledarklight() {
 	document.body.classList.toggle("toggle-darklight");
+	if (document.body.classList.contains("toggle-darklight")){
+		document.getElementById("icon").href = "../assets/favicon.svg"
+	}
+	else{
+		document.getElementById("icon").href = "../assets/favicon_dark.svg"
+	}
 }
 function overlay_off() {
 	document.getElementById("overlay").style.display = "none";
