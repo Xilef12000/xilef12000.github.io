54966efd27d039591ba49064405ebce0b9f8fb82
Xilef12000 <manuel.koenig@online.de>
2022-01-31 14:51:41 +0100

    Update installws2812-over-artnet.sh

--- demo/ws2812-over-artnet/installws2812-over-artnet.sh
+++ demo/ws2812-over-artnet/installws2812-over-artnet.sh
@@ -7,7 +7,7 @@ sudo apt-get install python3
 echo installing pip3
 sudo apt-get install python3-pip
 
-echo installing adafruit-neopixel (ws2812-library)
+echo installing adafruit neopixel (ws2812-library)
 sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 sudo python3 -m pip install --force-reinstall adafruit-blinka
 
