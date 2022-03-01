180f66bae818da6562de19d2513af9adf5e4ad66
Xilef12000 <manuel.koenig@online.de>
2022-03-01 17:21:08 +0100

    marked (green) links on 404 page

--- scripts/404.js
+++ scripts/404.js
@@ -1,5 +1,7 @@
 function onload404() {
 	onload();
-	document.getElementById("Url").innerHTML = "Requested Url: " + window.location.href;
+	//document.getElementById("Url").innerHTML = "Requested Url: " + window.location.href;
+	document.getElementById("Url").innerHTML = window.location.href;
+	document.getElementById("Url").href = window.location.href;
 	document.getElementById("Time").innerHTML = "Time of Request: " + Date();
 }
