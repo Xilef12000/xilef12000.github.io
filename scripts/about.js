29b28eff5073ea1f9e8c0c0211ec8aa42b31cb56
Xilef12000 <manuel.koenig@online.de>
2022-05-23 18:39:25 +0200

    changes

--- scripts/about.js
+++ scripts/about.js
@@ -1,5 +1,3 @@
 function onloadabout() {
-	onload();
-	document.getElementById("currentYearEN").innerHTML = new Date().getFullYear();
-	document.getElementById("currentYearDE").innerHTML = new Date().getFullYear();
+	document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 }
