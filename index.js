e6ce1ce27b30eabeca5f14e04e2afc6b79ace872
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-01-14 14:31:33 +0100

    hand out correct mime type

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
 const { readFile } = require('fs').promises;
+const { existsSync } = require('fs');
+const mime = require('mime');
+
+const assets = ['style-sheets', 'scripts', 'assets'];
 
 app.get('/', function(req, res) {
     res.render('pages/index');
@@ -15,7 +19,16 @@ app.get('/about', function(req, res) {
 
 
 app.get('*', async (req, res) => {
-    res.status(404).send( await readFile('./404.html', 'utf8') );
+    const url = req.originalUrl;
+    //console.log(url.split('/')[1]);
+    
+    if (assets.includes(url.split('/')[1]) && existsSync('.' + url)){
+        //console.log(url);
+        res.contentType(mime.getType(url.split('.').at(-1))).send( await readFile('.' + url, 'utf8') );
+    }
+    else {
+        res.status(404).send( await readFile('./404.html', 'utf8') );
+    }
 });
 
 app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
