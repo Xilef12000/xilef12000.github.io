8a49eb34e71d006dd988f139f002a4eaf8bbf4af
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-01-14 17:02:12 +0100

    not used

--- scripts/urlName.js
+++
@@ -1,7 +0,0 @@
-currentURL = window.location.href;
-args = document.currentScript.getAttribute('args');
-args = "?%20" + args;
-
-if (currentURL.split('/').includes('project') && currentURL.split('/').slice(-1) != args){
-    window.location.href = args;
-}
