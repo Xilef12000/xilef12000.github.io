fd547ee0d8c55e5dcb80f51cc27127a9ca55663a
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-01-24 19:12:38 +0100

    404

---
+++ docs/scripts/404.js
@@ -0,0 +1,3 @@
+document.getElementById("Url").innerHTML = window.location.href;
+document.getElementById("Url").href = window.location.href;
+document.getElementById("Time").innerHTML = "Time of Request: " + Date();
