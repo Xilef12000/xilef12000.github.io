955db336e2b1acb5b581ff6d9cb61e7e38625835
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-05-01 14:35:35 +0200

    added button to load external content

--- docs/scripts/imageflip.js
+++ docs/scripts/imageflip.js
@@ -7,3 +7,16 @@ document.querySelectorAll(".card__image").forEach(function(element) {
         element.parentNode.parentNode.parentNode.classList.add('is-flipped'); //for projects
     });
 });
+document.querySelectorAll(".card__image__instant").forEach(function(element) {
+        element.parentNode.parentNode.classList.add('is-flipped'); //for gallery
+        element.parentNode.parentNode.parentNode.classList.add('is-flipped'); //for projects
+});
+
+function loadExternal(element){
+    element.parentNode.outerHTML = element.parentNode.getAttribute("external")
+}
+function loadExternalAll(){
+    document.querySelectorAll(".external_content").forEach(function(element) {
+        element.outerHTML = element.getAttribute("external")
+    });
+}
