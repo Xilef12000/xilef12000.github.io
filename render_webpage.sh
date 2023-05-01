20760a858150c602c082bf64113a10f0bc2b8efe
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-05-01 13:03:11 +0200

    added fast mode

--- render_webpage.sh
+++ render_webpage.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
-# run: sh render_webpage.sh
+# run: "sh render_webpage.sh"
+# run: "sh render_webpage.sh -f" for fast render
+
+fast=false
+
+echo_help() {
+  echo "HELP: use -f for fast render mode"
+}
+
+while getopts 'fh?' flag; do
+  case "${flag}" in
+    f) fast=true ;;
+    *) echo_help
+       exit 1 ;;
+  esac
+done
+
 echo "\e[1;35m Preparing dir ... \e[0m"
 rm -r -f $(pwd)/docs/
 mkdir -p $(pwd)/docs/
@@ -9,8 +25,13 @@ fuser -k 3000/tcp
 node . > log_node.log &
 cp $(pwd)/CNAME $(pwd)/docs/CNAME
 echo "\e[1;35m Starting static render ... \e[0m"
-echo "\e[1;32m This might take a while, ingore the errors ... \e[0m"
-/bin/python3 $(pwd)/save-webpage.py -c render-conf.json -f > log_save-webpage.log
+if $fast ; then
+    echo "\e[1;32m Using fast mode \e[0m"
+    /bin/python3 $(pwd)/save-webpage.py -c render-conf.json -f > log_save-webpage.log
+else
+    echo "\e[1;32m This might take a while, ingore the errors ... \e[0m"
+    /bin/python3 $(pwd)/save-webpage.py -c render-conf.json > log_save-webpage.log
+fi
 echo "\e[1;35m cleaning up ... \e[0m"
 fuser -k 3000/tcp
 echo "\e[1;35m Starting HTTP.Server on 0.0.0.0 port 8000 (http://0.0.0.0:8000/) ... \e[0m"
