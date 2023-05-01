b5444a998bdde346c86152c54aa9ed76a237f2f7
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2023-05-01 12:07:41 +0200

    load config from config file

--- render_webpage.sh
+++ render_webpage.sh
@@ -10,7 +10,7 @@ node . > log_node.log &
 cp $(pwd)/CNAME $(pwd)/docs/CNAME
 echo "\e[1;35m Starting static render ... \e[0m"
 echo "\e[1;32m This might take a while, ingore the errors ... \e[0m"
-/bin/python3 $(pwd)/save-webpage.py > log_save-webpage.log
+/bin/python3 $(pwd)/save-webpage.py -c render-conf.json > log_save-webpage.log
 echo "\e[1;35m cleaning up ... \e[0m"
 fuser -k 3000/tcp
 echo "\e[1;35m Starting HTTP.Server on 0.0.0.0 port 8000 (http://0.0.0.0:8000/) ... \e[0m"
